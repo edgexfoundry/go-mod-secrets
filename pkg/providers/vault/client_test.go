1668bd969a0270753edaf957d7852ef715750299
lenny <leonard.goodell@intel.com>
2021-01-19 15:43:45 -0700

    refactor: Consume V2 go-mod-secrets
    close #81
    
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- pkg/providers/vault/client_test.go
+++ pkg/providers/vault/client_test.go
@@ -29,7 +29,7 @@ import (
 	"github.com/edgexfoundry/go-mod-secrets/v2/pkg"
 	"github.com/edgexfoundry/go-mod-secrets/v2/pkg/types"
 
-	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/logger"
 )
 
 var TestPath = "/data"
